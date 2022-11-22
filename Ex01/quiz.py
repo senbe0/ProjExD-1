7e713667edd75e9cc5611df82c26d45a98c7c336
takahashi <c0a2107793@edu.teu.ac.jp>
2022-11-22 16:31:36 +0900

    クイズ完成

--- Ex01/quiz.py
+++ Ex01/quiz.py
@@ -3,7 +3,6 @@ import datetime
 
 def shutudai():
     num = randint(0,2)
-    print(num)
     quiz_text = ["サザエの旦那の名前は？", "カツオの妹の名前は？", "タラオとカツオから見てどんな関係？"]
     quiz_ans = [["マスオ", "ますお"], ["ワカメ", "わかめ"], ["甥", "おい", "甥っ子", "おいっこ"]]
     print("問題："+ quiz_text[num])
@@ -20,6 +19,6 @@ def kaito(re_ans):
         ans_ms = "出直してこい"
     return ans_ms
 
-if __name__=="__mian__":
+if __name__ == "__main__":
     re_ans = shutudai()
     print( kaito(re_ans))
