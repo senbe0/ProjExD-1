d49011df2797fbdb4c34155fec5338db6e39a276
takahashi <c0a2107793@edu.teu.ac.jp>
2022-11-29 14:44:01 +0900

    3rd

--- Ex02/clac.py
+++ Ex02/clac.py
@@ -1,6 +1,11 @@
 import tkinter as tk
 import tkinter.messagebox as tkm
 
+def button_click(event):
+    btn = event.widget
+    num = btn["text"]
+    tkm.showinfo("", f"{num}ボタンがクリックされました")
+
 root = tk.Tk()
 root.title("電卓")
 root.geometry("300x500")
@@ -9,7 +14,7 @@ r,c = 0,0
 for i in range(9, -1, -1):
     button =tk.Button(root, text=f"{i}", width=4, height=2, font=("", 30))
     button.grid(row=r, column=c)
-    #button.bimd("<1>", button_clik)
+    button.bind("<1>", button_click)
     c += 1
     if c%3 == 0:
         r += 1
