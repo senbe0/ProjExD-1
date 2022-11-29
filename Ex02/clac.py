206cd62e4f5110516b7b2b187af124923c1af556
takahashi <c0a2107793@edu.teu.ac.jp>
2022-11-29 14:20:42 +0900

    ２回目

---
+++ Ex02/clac.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+import tkinter.messagebox as tkm
+
+root = tk.Tk
+root.title("tk")
+root.geometry("300x500")
+for i in range(9):
+    button =tk.Button(root, text=i)
+    button.width(4)
