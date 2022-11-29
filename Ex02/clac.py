20a5b50d01041efaa06700c10e89143d28cdf603
takahashi <c0a2107793@edu.teu.ac.jp>
2022-11-29 14:41:56 +0900

    2回目

--- Ex02/clac.py
+++ Ex02/clac.py
@@ -1,11 +1,18 @@
 import tkinter as tk
 import tkinter.messagebox as tkm
 
-root = tk.Tk
-root.title("tk")
+root = tk.Tk()
+root.title("電卓")
 root.geometry("300x500")
 
+r,c = 0,0
+for i in range(9, -1, -1):
+    button =tk.Button(root, text=f"{i}", width=4, height=2, font=("", 30))
+    button.grid(row=r, column=c)
+    #button.bimd("<1>", button_clik)
+    c += 1
+    if c%3 == 0:
+        r += 1
+        c = 0
 
-for i in range(9):
-    button =tk.Button(root, text=i)
-    button.width(4)
+root.mainloop()
