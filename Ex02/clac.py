6daf24258ae2cb6948657b8c7f29eccbe60d7f1a
takahashi <c0a2107793@edu.teu.ac.jp>
2022-11-29 15:07:09 +0900

    4th

--- Ex02/clac.py
+++ Ex02/clac.py
@@ -4,15 +4,34 @@ import tkinter.messagebox as tkm
 def button_click(event):
     btn = event.widget
     num = btn["text"]
-    tkm.showinfo("", f"{num}ボタンがクリックされました")
+    if num == "=":
+        siki = entry.get() # 数式の文字列
+        res = eval(siki) # 数式文字列の評価
+        entry.delete(0, tk.END) # 表示文字列の削除
+        entry.insert(tk.END, res) # 結果の挿入
+    else: 
+        entry.insert(tk.END, num)
 
+    
 root = tk.Tk()
-root.title("電卓")
 root.geometry("300x500")
 
-r,c = 0,0
-for i in range(9, -1, -1):
-    button =tk.Button(root, text=f"{i}", width=4, height=2, font=("", 30))
+entry = tk.Entry(root, justify="right", width=10, font=("",40))
+entry.grid(row=0, column=0, columnspan=3)
+
+r, c = 1, 0
+for num in range(9, -1, -1):
+    button = tk.Button(root, text=f"{num}", width=4, height=2, font=("", 30))
+    button.grid(row=r, column=c)
+    button.bind("<1>", button_click)
+    c += 1
+    if c%3 == 0:
+        r += 1
+        c = 0
+
+operators = ["+", "="]
+for ope in operators:
+    button = tk.Button(root, text=f"{ope}", width=4, height=2, font=("", 30))
     button.grid(row=r, column=c)
     button.bind("<1>", button_click)
     c += 1
